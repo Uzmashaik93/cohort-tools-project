cb5cc8cba201f03fb2bb06ce44ca98e4c572527e
uzma shaik <uzma97910@gmail.com>
2025-02-25 16:59:07 +0100

    install mongoose and schemas

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const cors = require('cors');
+const cors = require("cors");
+
+const Cohort = require("./models/cohort.model");
+const Student = require("./models/student.model");
 
 const cohorts = require("./cohorts.json");
 const students = require("./students.json");
@@ -11,17 +14,24 @@ const PORT = 5005;
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
-
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
+
 app.use(
   cors({
-    origin: ['http://localhost:5005', 'http://example.com'], // Add the URLs of allowed origins to this array
+    origin: ["http://localhost:5005", "http://example.com"], // Add the URLs of allowed origins to this array
   })
 );
 app.use(express.json());
@@ -30,7 +40,6 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
@@ -38,17 +47,13 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 app.get("/api/cohorts", (req, res) => {
-  res.json(cohorts)
+  res.json(cohorts);
 });
 app.get("/api/students", (req, res) => {
-  res.json(students)
+  res.json(students);
 });
-
-
-
 
 // START SERVER
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
